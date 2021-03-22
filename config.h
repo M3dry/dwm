f03004a031f50cb6da619c673fb5b69f59dbfe17
M3dry <M3dry@protonmail.com>
2021-03-22 19:24:05 +0100

    Changing some stuff

--- config.h
+++ config.h
@@ -1,12 +1,6 @@
-/* See LICENSE file for copyright and license details. */
-
-/* appearance */
-/*  Display modes of the tab bar: never shown, always shown, shown only in  */
-/*  monocle mode in the presence of several windows.                        */
-/*  Modes after showtab_nmodes are disabled.                                */
 enum showtab_modes { showtab_never, showtab_auto, showtab_nmodes, showtab_always};
-static const int showtab			     = showtab_auto; /* Default tab bar show mode */
-static const int toptab				     = 0;   /* False means bottom tab bar */
+static const int showtab                 = showtab_auto; /* Default tab bar show mode */
+static const int toptab                  = 0;   /* False means bottom tab bar */
 static const unsigned int borderpx       = 2;   /* border pixel of windows */
 static const unsigned int snap           = 0;   /* snap pixel */
 static const int swallowfloating         = 1;   /* 1 means swallow floating windows by default */
@@ -22,9 +16,9 @@ static const int topbar                  = 1;   /* 0 means bottom bar */
 static const int horizpadbar             = 8;   /* horizontal padding for statusbar */
 static const int vertpadbar              = 0;   /* vertical padding for statusbar */
 static const int user_bh                 = 24;  /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const unsigned int ulinepad	     = 2;   /* horizontal padding between the underline and tag */
-static const unsigned int ulinestroke	 = 2;   /* thickness / height of the underline */
-static const unsigned int ulinevoffset	 = 0;   /* how far above the bottom of the bar the line should appear */
+static const unsigned int ulinepad       = 2;   /* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke    = 2;   /* thickness / height of the underline */
+static const unsigned int ulinevoffset   = 0;   /* how far above the bottom of the bar the line should appear */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
@@ -89,19 +83,18 @@ static const char *colors[][10]  = {
 	/* Tags/borders       fg            bg      float               sticky            sticky + float         fakefullscreen   fakefullscreen + float */
 	[SchemeNorm]        = { normfg,     normbg, normfloatwinborder, normstickyborder, normstickyfloatborder, normfakefullscr, normfakefullscrfloat },
 	[SchemeSel]         = { selfg,      selbg,  selfloatwinborder,  selstickyborder,  selstickyfloatborder,  selfakefullscr,  selfakefullscrfloat },
-	[SchemeOccupied]    = { occupiedfg, occupiedbg }, /* Tags that have 1 of more windowns */
-	[SchemeOccupiedInv] = { ocinvfg,    ocinvbg }, /* Tags that have 1 of more windowns */
-	[SchemeStatus]      = { statusfg,   statusbg }, /* Status / ltsymbol */
-	[SchemeTabNorm]     = { normtabfg,  normtabbg }, /* Tabs */
-	[SchemeTabSel]      = { seltabfg,   seltabbg}, /* Tabs selected */
-	[SchemeClientSel]   = { selindfg }, /* Client indicators */
-	[SchemeClientNorm]  = { normindfg }, /* Client indicators unfocused */
-	[SchemeClientInc]   = { incindfg }, /* Client indicators unfocused */
-	[SchemeInvMon]      = { invmonfg,    invmonbg }, /* Unfocused monitors */
+	[SchemeOccupied]    = { occupiedfg, occupiedbg },
+	[SchemeOccupiedInv] = { ocinvfg,    ocinvbg },
+	[SchemeStatus]      = { statusfg,   statusbg },
+	[SchemeTabNorm]     = { normtabfg,  normtabbg },
+	[SchemeTabSel]      = { seltabfg,   seltabbg},
+	[SchemeClientSel]   = { selindfg },
+	[SchemeClientNorm]  = { normindfg },
+	[SchemeClientInc]   = { incindfg },
+	[SchemeInvMon]      = { invmonfg,    invmonbg },
 	/* Win borders          tile            fibonacci            float            deck            nrowgrid            bstack            centeredmaster       monocle            gaplessgrid */
 	[SchemeNormLayout]  = { normtileborder, normfibonacciborder, normfloatborder, normdeckborder, normnrowgridborder, normbstackborder, normcenmasterborder, normmonocleborder, normgaplessgridborder },
 	[SchemeSelLayout]   = { seltileborder,  selfibonacciborder,  selfloatborder,  seldeckborder,  selnrowgridborder,  selbstackborder,  selcenmasterborder,  selmonocleborder,  selgaplessgridborder },
-    /* if you change layout order you also need to change the order here */
 };
 
 typedef struct {
@@ -119,7 +112,6 @@ static Sp scratchpads[] = {
    {"spcal",       spcmd3},
 };
 
-/* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 #define WTYPE "_NET_WM_WINDOW_TYPE_"
@@ -131,9 +123,9 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title          wintype    tags mask     switchtotag     iscentered   isfloating   ispermanent   isterminal    noswallow   monitor */
     /* Scratchpads */
-	{ "spterm",	  NULL,	      NULL,	         NULL,      SPTAG(0),	  0,              1,           1,			0,            0,            0,          -1 }, /* St */
-	{ "spmus",	  NULL,	      NULL,	         NULL,      SPTAG(1),	  0,              1,           1,			0,            0,            0,          -1 }, /* cmus */
-	{ NULL,		  NULL,	      "spcal",	     NULL,      SPTAG(2),	  0,              1,           1,			0,            0,            0,          -1 }, /* qalculate-gtk */
+	{ "spte rm",  NULL,       NULL,          NULL,      SPTAG(0),	  0,              1,           1,           0,            0,            0,          -1 }, /* St */
+	{ "spmus",    NULL,       NULL,          NULL,      SPTAG(1),	  0,              1,           1,           0,            0,            0,          -1 }, /* cmus */
+	{ NULL,       NULL,       "spcal",       NULL,      SPTAG(2),	  0,              1,           1,           0,            0,            0,          -1 }, /* qalculate-gtk */
     /* Terminals */
 	{ "St",       NULL,       NULL,          NULL,      0,            0,              0,           0,           0,            1,            0,          -1 },
 	{ "Alacritty",NULL,       NULL,          NULL,      0,            0,              0,           0,           0,            1,            0,          -1 },
@@ -158,15 +150,11 @@ static const MonitorRule monrules[] = {
 	{  -1,      -1,  0,      -1,    -1,      -1,      -1     }, // default
 };
 
-/* layout(s) */
-static const float mfact     = 0.5;  /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5;
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int attachbelow = 1;    /* 1 means attach after the currently active window */
-
-#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
-
-#include <X11/XF86keysym.h>
+static const int nmaster     = 1;
+static const int attachbelow = 1;
+#define FORCE_VSPLIT 1
 #include "vanitygaps.c"
 
 static const Layout layouts[] = {
@@ -183,12 +171,13 @@ static const Layout layouts[] = {
 	{ NULL,       NULL },
 };
 
-/* key definitions */
+#include <X11/XF86keysym.h>
+
 #define M Mod4Mask
 #define A Mod1Mask
 #define S ShiftMask
-
 #define C ControlMask
+
 #define TAGKEYS(CHAIN,KEY,TAG) \
 	{ A,       CHAIN,   KEY,   comboview,    {.ui = 1 << TAG} }, \
 	{ C,       CHAIN,   KEY,   toggleview,   {.ui = 1 << TAG} }, \
@@ -199,13 +188,9 @@ static const Layout layouts[] = {
 	{ A|M,     CHAIN,   KEY,   tagnextmon,   {.ui = 1 << TAG} }, \
 	{ A|M|S,   CHAIN,   KEY,   tagprevmon,   {.ui = 1 << TAG} },
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-
 static Key keys[] = {
-	/* modifier     chain   key            function                argument */
-    /* Spawing preograms */
 	{ A,            -1,     XK_Return,     spawn,                  SHCMD("$TERMINAL") },
 	{ A|S,          -1,     XK_c,          spawn,                  SHCMD("$TERMINAL htop") },
 	{ A|S,          -1,     XK_z,          spawn,                  SHCMD("playerctl play-pause") },
@@ -224,7 +209,7 @@ static Key keys[] = {
 	{ C,            -1,     XK_m,          spawn,                  SHCMD("multimc") },
 	{ M|C|A,        -1,     XK_l,          spawn,                  SHCMD("slock") },
 	{ C|A,          -1,     XK_z,          spawn,                  SHCMD("playerctl play-pause") },
-    /* Dmenu scripts */
+
 	{ A|S,          -1,     XK_Return,     spawn,                  SHCMD("dmenu_run -l 5 -g 10 -p 'Run:'") },
 	{ A,            -1,     XK_c,          spawn,                  SHCMD("volume-script") },
 	{ A|C,          -1,     XK_Return,     spawn,                  SHCMD("Booky 'emacsclient -c -a emacs' '><' 'Cconfig'") },
@@ -237,13 +222,13 @@ static Key keys[] = {
 	{ A|S,          XK_d,   XK_v,          spawn,                  SHCMD("manview") },
 	{ A|S,          XK_d,   XK_a,          spawn,                  SHCMD("allmenu") },
 	{ A|S,          XK_d,   XK_q,          spawn,                  SHCMD("shut") },
-    /* MultiMedia keys */
+
 	{ 0,-1, XF86XK_AudioPrev,              spawn,                  SHCMD("playerctl --player cmus previous") },
 	{ 0,-1, XF86XK_AudioNext,              spawn,                  SHCMD("playerctl --player cmus next") },
 	{ 0,-1, XF86XK_AudioPlay,              spawn,                  SHCMD("playerctl --player cmus play-pause") },
 	{ 0,-1, XF86XK_AudioLowerVolume,       spawn,                  SHCMD("pamixer --allow-boost -d 1 ; killall dwmStatus && dwmStatus &") },
 	{ 0,-1, XF86XK_AudioRaiseVolume,       spawn,                  SHCMD("pamixer --allow-boost -i 1 ; killall dwmStatus && dwmStatus &") },
-    /* DWM keybindings */
+
 	{ A,            -1,     XK_q,          killclient,             {0} },
 	{ A|C|S,        -1,     XK_x,          killpermanent,          {0} },
 	{ A|S,          -1,     XK_q,          killunsel,              {0} },
@@ -265,7 +250,7 @@ static Key keys[] = {
 	{ M|S,          -1,     XK_k,          focusstack,             {.i = -1 } },
 	{ M|C,          -1,     XK_j,          inplacerotate,          {.i = +2 } },
 	{ M|C,          -1,     XK_k,          inplacerotate,          {.i = -2 } },
-    /* Layouts */
+
 	{ A,            -1,     XK_t,          setlayout,              {.v = &layouts[0]} },
 	{ A,            -1,     XK_v,          setlayout,              {.v = &layouts[1]} },
 	{ A|S,          -1,     XK_f,          setlayout,              {.v = &layouts[2]} },
@@ -283,7 +268,7 @@ static Key keys[] = {
 	{ A|S,          -1,     XK_n,          shiftviewclients,       { .i = +1 } },
 	{ A|S,          -1,     XK_p,          shiftviewclients,       { .i = -1 } },
 	{ A|S,          -1,     XK_a,          winview,                {0} },
-    /* Window manipulation */
+
 	{ A,            -1,     XK_semicolon,  zoom,                   {0} },
 	{ A|S,          -1,     XK_v,          transfer,               {0} },
 	{ M,            -1,     XK_j,          pushdown,               {0} },
@@ -296,12 +281,12 @@ static Key keys[] = {
 	{ A,            -1,     XK_u,          togglescratch,          {.ui = 0 } },
     { A,            -1,     XK_i,          togglescratch,          {.ui = 1 } },
     { A,            -1,     XK_y,          togglescratch,          {.ui = 2 } },
-    /* Monitors */
+
 	{ A,            -1,     XK_comma,      focusmon,               {.i = -1 } },
 	{ A,            -1,     XK_period,     focusmon,               {.i = +1 } },
 	{ A|S,          -1,     XK_comma,      tagmon,                 {.i = -1 } },
 	{ A|S,          -1,     XK_period,     tagmon,                 {.i = +1 } },
-    /* Moveresize */
+
 	{ A|C,          -1,     XK_j,          moveresize,             {.v = "0x 25y 0w 0h" } },
 	{ A|C,          -1,     XK_k,          moveresize,             {.v = "0x -25y 0w 0h" } },
 	{ A|C,          -1,     XK_l,          moveresize,             {.v = "25x 0y 0w 0h" } },
@@ -310,15 +295,16 @@ static Key keys[] = {
 	{ M|C,          -1,     XK_k,          moveresize,             {.v = "0x 0y 0w -25h" } },
 	{ M|C,          -1,     XK_l,          moveresize,             {.v = "0x 0y 25w 0h" } },
 	{ M|C,          -1,     XK_h,          moveresize,             {.v = "0x 0y -25w 0h" } },
-    /* Gaps */
+
 	{ A|S,          -1,     XK_equal,      incrgaps,               {.i = +1 } },
 	{ A|S,          -1,     XK_minus,      incrgaps,               {.i = -1 } },
 	{ A|S,          -1,     XK_0,          defaultgaps,            {0} },
 	{ A|C,          -1,     XK_0,          togglegaps,             {0} },
-    /* Borderpx */
+
 	{ A|C,          -1,     XK_equal,      setborderpx,            {.i = +1 } },
 	{ A|C,          -1,     XK_minus,      setborderpx,            {.i = -1 } },
 	{ M,            -1,     XK_0,          setborderpx,            {.i = 0 } },
+
 	TAGKEYS(        -1,     XK_1,                                  0)
 	TAGKEYS(        -1,     XK_2,                                  1)
 	TAGKEYS(        -1,     XK_3,                                  2)
@@ -328,12 +314,11 @@ static Key keys[] = {
 	TAGKEYS(        -1,     XK_7,                                  6)
 	TAGKEYS(        -1,     XK_8,                                  7)
 	TAGKEYS(        -1,     XK_9,                                  8)
-	{ M|S,          -1,     XK_Escape,     quit,                   {0} }, 
-	{ A|C|S,        -1,     XK_q,          quit,                   {1} }, 
+
+	{ M|S,          -1,     XK_Escape,     quit,                   {0} },
+	{ A|C|S,        -1,     XK_q,          quit,                   {1} },
 };
 
-/* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkClientWin,         A,              Button1,        movemouse,      {0} },
